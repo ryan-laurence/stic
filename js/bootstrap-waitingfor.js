fdf394e9c83247de764e355b1dedff928ff87c90
Ryan Q. Pesigan <ryan.laurence@gmail.com>
2016-07-14 19:15:38 +0800

    Update some files.

--- js/bootstrap-waitingfor.js
+++ js/bootstrap-waitingfor.js
@@ -2,7 +2,7 @@
  * Module for displaying "Waiting for..." dialog using Bootstrap
  *
  * @author Eugene Maslovich <ehpc@em42.ru>
- * 
+ *
  */
 
 (function (root, factory) {
@@ -48,21 +48,21 @@
 
 	// Dialog object
 	var $dialog,config,cache={};
-        
+
 	return {
 		/**
 		 * Opens our dialog
 		 * @param message Custom message
 		 * @param options Custom options:
-		 *   options.headerText - if the option is set to boolean false, 
+		 *   options.headerText - if the option is set to boolean false,
 		 *     it will hide the header and "message" will be set in a paragraph above the progress bar.
-		 *     When headerText is a not-empty string, "message" becomes a content 
+		 *     When headerText is a not-empty string, "message" becomes a content
 		 *     above the progress bar and headerText string will be set as a text inside the H3;
 		 *   options.headerSize - this will generate a heading corresponding to the size number. Like <h1>, <h2>, <h3> etc;
 		 *   options.headerClass - extra class(es) for the header tag;
 		 *   options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
 		 *   options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning";
-		 *   options.contentElement - determines the tag of the content element. 
+		 *   options.contentElement - determines the tag of the content element.
 		 *     Defaults to "p", which will generate a <p> tag;
 		 *   options.contentClass - extra class(es) for the content tag.
 		 */
@@ -163,32 +163,32 @@
 		/**
 		 * animate the  message every period equals to 'timer',
 		 * and starts this animation after a delay equals to 'timeout'
-		 * 
-		 * 
-		 * @param messages can be  : 
+		 *
+		 *
+		 * @param messages can be  :
 		 *      - string : it will be an array , i.e: messages="waitings"--> messages=["waiting..","waiting....",""waiting"......"]
-		 *      - array 
-		 *      - function 
+		 *      - array
+		 *      - function
 		 * @param timer period
 		 * @param timeout if it is 0 -> starts immediatly
-		 * 
+		 *
 		 * @return id of periodic job
 		 * @author Abdennour TOUMI <abdennour.toumi@gmail.com>
 		 * */
 		,animate:function(messages,timer,timeout){
 			timer=timer ||config.timer;
 			timeout=timeout||config.timeout;
-			
+
 			messages=messages||$dialog.find('.modal-header>h'+config.headerSize).html();
 			cache.animate=cache.animate || [];
 			if(typeof messages ==='string'){
-			     	
-			   	
+
+
 			     messages=['..','....','......'].map(function(e){
 			     	return messages+e;
 			     })	;
 			}
-			
+
 			if(typeof messages ==='object' && messages instanceof Array){
 				var old=messages;
 				messages=function(container){
@@ -196,14 +196,14 @@
 					if(current<0){
 						container.html(old[0]);
 					}else{
-					      var indx=(current+1>=old.length)?0:current+1	
-					       container.html(old[indx]);	
-						
+					      var indx=(current+1>=old.length)?0:current+1
+					       container.html(old[indx]);
+
 					}
 				}
-				
+
 			}
-		
+
 			if(typeof messages ==="function"){
 				if(timeout<timer){
 				        setTimeout(function(){
@@ -216,17 +216,17 @@
 				cache.animate.push(job);
 				return job;
 			}
-			
-			
-			
+
+
+
 		},
 		/**
 		* stop job with specified id .
-		   if no id specified , stopAnimate will stop the last running job . 
+		   if no id specified , stopAnimate will stop the last running job .
 		*@param id
 		* @author Abdennour TOUMI <abdennour.toumi@gmail.com>
 		*/
-		 
+
 		stopAnimate:function(id){
 			 id=id || cache.animate[cache.animate.length-1];
 			 clearInterval(id);
@@ -236,10 +236,10 @@
 		/**
 		* @param percentOrCurrent
 		* @param total
-		* Calling with : 
+		* Calling with :
 		*   - No Arguement --> get the current progress
 		*   - One Argument --> "percentOrCurrent" is the percent of progress
-		*   - Two Argument ---> "percentOrCurrent" is the current amount, "total" is the total amount . 
+		*   - Two Argument ---> "percentOrCurrent" is the current amount, "total" is the total amount .
 		*@author Abdennour TOUMI <abdennour.toumi@gmail.com>
 		*/
 		progress:function(percentOrCurrent,total){
@@ -251,7 +251,7 @@
 			}
 			$dialog.find('.progress-bar').css('width',percentOrCurrent+'%');
 			return $dialog;
-		}		
+		}
 	};
 
 }));
